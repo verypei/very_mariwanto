2752e0dbf851b45afcee4dfeddc9b364083bbbba
verypei <verrymariyanto@gmail.com>
2021-06-02 19:17:01 +0700

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("kalimat").style.backgroundColor = "blue"
+}
+// innerHTML = mengubah isi value dalam tag html 
+// innner text mengabil data dari element html
